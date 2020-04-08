a50fe662b4890525c4885b554783acb2657b7c82
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-04-08 21:46:00 +0530

    set rating to two decimals

--- src/Components/Browse/RecipeCard.jsx
+++ src/Components/Browse/RecipeCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const RecipeCard = props => {
+const RecipeCard = (props) => {
   const { cuisine } = props;
   return (
     <div className="recipe__card">
       <div className="recipe__title">
         <h5 className="recipe__name">{cuisine["name"]}</h5>
-        <p className="recipe__rating">{cuisine["ratings"] + " Stars"}</p>
+        <p className="recipe__rating">
+          {parseFloat(cuisine["ratings"]).toFixed(2) + " Stars"}
+        </p>
       </div>
       <div className="recipe__content">
         <div className="recipe__image">
