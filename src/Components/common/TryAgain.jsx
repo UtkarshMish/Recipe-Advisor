5aa241eaf9e8c6d32552a85187f78e164dc4ebaf
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-07-30 14:49:26 +0530

    updated try again structure

--- src/Components/common/TryAgain.jsx
+++ src/Components/common/TryAgain.jsx
@@ -5,7 +5,9 @@ const TryAgain = () => {
   return (
     <div className="tryAgain__container">
       <div className="tryAgain__body">
-        <h2 className="tryAgain__message">No New {name} ! Try Again Later ?</h2>
+        <h2 className="tryAgain__message">
+          No Page for {String(name).replace(/-/g, " ")} ! Come Back Later ?
+        </h2>
       </div>
     </div>
   );
